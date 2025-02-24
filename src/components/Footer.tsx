20da20465945e2752725b806cb933f2fc82ed05d
Amith B V <amithrao0606@gmail.com>
2025-02-24 16:35:38 +0530

    Created a footer component.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-between w-[972px] items-center text-[15px] opacity-30 mt-[12px]">
+      <small className="text-[15px]">&copy; 2025. Copyright by Amith</small>
+      <p>
+        Version <b>1.5</b>
+      </p>
+    </footer>
+  );
+}
